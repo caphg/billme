9f56be05d8baa058690b4dbcc641daaf7844a931
Hrvoje G <hrvoje.grdic1@gmail.com>
2016-02-16 15:32:40 +0100

    file name update

--- lib/ruby_bill.rb
+++ lib/ruby_bill.rb
@@ -1,8 +1,6 @@
 require "ruby_bill/version"
 require_relative "ruby_bill/entries"
-require_relative "ruby_bill/generator"
-require_relative "ruby_bill/group"
-require_relative "ruby_bill/section"
+require_relative "ruby_bill/factory"
 require_relative "ruby_bill/sub_section"
 
 module RubyBill
