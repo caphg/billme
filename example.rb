e9c7a383b8b77d4776eddc089f308dd23609bbc8
Hrvoje G <hrvoje.grdic1@gmail.com>
2016-02-19 13:10:07 +0100

    update example

--- example.rb
+++ example.rb
@@ -7,35 +7,35 @@ RubyBill.bill do
   company do
     logo "logo.png"
 
-    company_name "Bitlab d.o.o."
-    company_address "Adress 123"
-    company_city "City 123"
-    company_country "Great country"
+    company_name "Enterprise LLC"
+    company_address "The Neutral Zone 123"
+    company_city "Beta Quadrant"
+    company_country "Universe"
     company_phone "+ 123 123 123 1"
-    company_email "me@coolguy.com"
+    company_email "uss@ncc1701.com"
   end
 
   client do
-    project_name "Website development"
-    name "google"
+    project_name "Battle engagement"
+    name "Federation"
     date "2015-05-05"
     due_date "2015-06-05"
-    address "California"
-    email "google@google.com"
+    address "Earth"
+    email "starfleet@earth.com"
   end
 
   services do
     tax "0.25"
     service do
-      name "Website design"
-      description "Designing period 2015-5-5 to 2015-6-5"
+      name "Engage Romulans"
+      description "Warmup for battle"
       unit "1000"
       currency "$"
       quantity "1"
     end
     service do
-      name "Backend stuff"
-      description "Period 2015-5-5 to 2015-6-5"
+      name "Rescue mission"
+      description "Rescue captive away teams"
       unit "2000"
       currency "$"
       quantity "2"
