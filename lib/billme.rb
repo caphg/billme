e2fd8f69f5e021b80b514bbb4ac6d90ee02a2f98
Hrvoje G <hrvoje.grdic1@gmail.com>
2016-02-26 15:28:22 +0100

    i18n init

--- lib/billme.rb
+++ lib/billme.rb
@@ -4,6 +4,12 @@ require_relative "billme/factory"
 require_relative "billme/section"
 require_relative "billme/services_section"
 require_relative "billme/service_details"
+require 'i18n'
+
+lang_path = File.expand_path('../../config', __FILE__)
+I18n.load_path = Dir["#{lang_path}/locales/*.yml"]
+I18n.backend.load_translations
+I18n.default_locale = :en
 
 module Billme
   def self.bill(&block)
